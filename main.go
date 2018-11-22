bf88909be6e8e38327db5db0ae0085d2a924f949
mrmagooey <mrmagooey@users.noreply.github.com>
2018-11-22 15:52:48 +1100

    cleanup, get username and ssh version

--- main.go
+++ main.go
@@ -6,13 +6,11 @@ import (
 	"log"
 	"net"
 	"os"
-	"os/exec"
-	"syscall"
+	"reflect"
 
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
-	"encoding/binary"
 	"encoding/pem"
 
 	"github.com/kr/pty"
@@ -55,163 +53,26 @@ func generateSSHKeys() (pub []byte, priv []byte, err error) {
 
 // https://github.com/Scalingo/go-ssh-examples/blob/master/server_complex.go
 
-func ptyRun(c *exec.Cmd, tty *os.File) (err error) {
-	defer tty.Close()
-	c.Stdout = tty
-	c.Stdin = tty
-	c.Stderr = tty
-	c.SysProcAttr = &syscall.SysProcAttr{
-		Setctty: true,
-		Setsid:  true,
-	}
-	return c.Start()
-}
-
-// parseDims extracts two uint32s from the provided buffer.
-func parseDims(b []byte) (uint32, uint32) {
-	w := binary.BigEndian.Uint32(b)
-	h := binary.BigEndian.Uint32(b[4:])
-	return w, h
-}
-
-// Winsize stores the Height and Width of a terminal.
-type Winsize struct {
-	Height uint16
-	Width  uint16
-	x      uint16 // unused
-	y      uint16 // unused
-}
-
-// SetWinsize sets the size of the given pty.
-// func SetWinsize(fd uintptr, w, h uint32) {
-// 	log.Printf("window resize %dx%d", w, h)
-// 	ws := &Winsize{Width: uint16(w), Height: uint16(h)}
-// 	syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TIOCSWINSZ), uintptr(unsafe.Pointer(ws)))
-// }
-
 func handleExec() bool {
-	// command := string(req.Payload[4 : req.Payload[3]+4])
-
-	// cmd := exec.Command(shell, []string{"-c", command}...)
-
-	// cmd.Stdout = channel
-	// cmd.Stderr = channel
-	// cmd.Stdin = channel
-
-	// err := cmd.Start()
-	// if err != nil {
-	// 	log.Printf("could not start command (%s)", err)
-	// 	continue
-	// }
-
-	// // teardown session
-	// go func() {
-	// 	_, err := cmd.Process.Wait()
-	// 	if err != nil {
-	// 		log.Printf("failed to exit bash (%s)", err)
-	// 	}
-	// 	channel.Close()
-	// 	log.Printf("session closed")
-	// }()
-
 	return true
 }
 
 func handlePty() bool {
-	// Parse body...
-	// termLen := req.Payload[3]
-	// termEnv := string(req.Payload[4 : termLen+4])
-	// w, h := parseDims(req.Payload[termLen+4:])
-	// SetWinsize(f.Fd(), w, h)
-	// log.Printf("pty-req '%s'", termEnv)
 	return true
 }
 
 func handleEnv(req *ssh.Request) bool {
-	// Parse body...
-	// termLen := req.Payload[3]
-	// termEnv := string(req.Payload[4 : termLen+4])
-	// w, h := parseDims(req.Payload[termLen+4:])
-	// SetWinsize(f.Fd(), w, h)
-	// log.Printf("pty-req '%s'", termEnv)
 	return true
 }
 
-func handleShell(channel ssh.Channel, req *ssh.Request, tty *os.File, f *os.File) bool {
+func handleShell(channel ssh.Channel, req *ssh.Request, tty *os.File, f *os.File, connDetails map[string]string) bool {
 	// gnomeKeychainTrick(channel)
 	signalsTrick(channel)
-	// line, more, err := bio.ReadLine()
-
-	// fmt.Print(more)
-	// fmt.Print(err)
-	// s := string(line)
-	// fmt.Println(s)
-
-	// in case you need a string which contains the newline
-	// s, err := bio.ReadString('\n')
-	// fmt.Println(s)
-
-	//	ioutil.ReadAll(channel)
-
-	// scanner := bufio.NewScanner(channel) //
-
-	//	scanner.Split(bufio.ScanWords)
-
-	// for scanner.Scan() {
-	// 	fmt.Println("c")
-	// 	line := scanner.Text()
-	// 	fmt.Println("c")
-	// 	if line == "\n" {
-	// 		break
-	// 	}
-	// 	fmt.Println(line)
-
-	// }
-
-	// if b, err := ioutil.ReadAll(channel); err == nil {
-	// 	fmt.Println(string(b))
-	// }
-
-	// buf := new(bytes.Buffer)
-	// buf.ReadFrom(channel)
-
-	// s := buf.String()
-	// fmt.Println("c")
-	// fmt.Println(s)
-
-	// shell := defaultShell
-	// cmd := exec.Command(shell)
-	// cmd.Env = []string{"TERM=xterm"}
-	// err := ptyRun(cmd, tty)
-	// if err != nil {
-	// 	log.Printf("%s", err)
-	// }
-	// // Teardown session
-	// var once sync.Once
-	// close := func() {
-	// 	channel.Close()
-	// 	log.Printf("session closed")
-	// }
-	// // Pipe session to bash and visa-versa
-	// go func() {
-	// 	io.Copy(channel, f)
-	// 	once.Do(close)
-	// }()
-	// go func() {
-	// 	io.Copy(f, channel)
-	// 	once.Do(close)
-	// }()
-	// // We don't accept any commands (Payload),
-	// // only the default shell.
-	// if len(req.Payload) == 0 {
-	// 	return true
-	// }
-
 	return true
 
 }
 
-func handleRequest(in <-chan *ssh.Request, channel ssh.Channel, tty *os.File, f *os.File) error {
+func handleRequest(in <-chan *ssh.Request, channel ssh.Channel, tty *os.File, f *os.File, connDetails map[string]string) error {
 	for req := range in {
 		// log.Printf("%v %s", req.Payload, req.Payload)
 		ok := false
@@ -219,7 +80,7 @@ func handleRequest(in <-chan *ssh.Request, channel ssh.Channel, tty *os.File, f 
 		case "exec":
 			ok = handleExec()
 		case "shell":
-			ok = handleShell(channel, req, tty, f)
+			ok = handleShell(channel, req, tty, f, connDetails)
 		case "pty-req":
 			ok = handlePty()
 		case "window-change":
@@ -235,7 +96,7 @@ func handleRequest(in <-chan *ssh.Request, channel ssh.Channel, tty *os.File, f 
 	return nil
 }
 
-func handleChannels(chans <-chan ssh.NewChannel) {
+func handleChannels(chans <-chan ssh.NewChannel, connDetails map[string]string) {
 	// Service the incoming Channel channel.
 	for newChannel := range chans {
 		if t := newChannel.ChannelType(); t != "session" {
@@ -252,8 +113,21 @@ func handleChannels(chans <-chan ssh.NewChannel) {
 			log.Printf("could not start pty (%s)", err)
 			continue
 		}
-		go handleRequest(requests, channel, tty, f)
+		go handleRequest(requests, channel, tty, f, connDetails)
 	}
+}
+
+func getUsername(f *ssh.Conn) string {
+	v := reflect.ValueOf(*f)
+	y := v.Elem().FieldByName("sshConn")
+	return y.FieldByName("user").String()
+}
+
+func getClientSSHVersion(f *ssh.Conn) string {
+	v := reflect.ValueOf(*f)
+	y := v.Elem().FieldByName("sshConn")
+	clientVersion := y.FieldByName("clientVersion").Bytes()
+	return string(clientVersion)
 }
 
 func main() {
@@ -296,60 +170,23 @@ func main() {
 
 	// Before use, a handshake must be performed on the incoming
 	// net.Conn.
-	_, chans, reqs, err := ssh.NewServerConn(nConn, config)
+
+	conn, chans, reqs, err := ssh.NewServerConn(nConn, config)
 	if err != nil {
-		log.Fatal("failed to handshake: ", err)
+		log.Println("failed to handshake: ", err)
+	}
+
+	username := getUsername(&conn.Conn)
+	sshVersion := getClientSSHVersion(&conn.Conn)
+	connectionDetails := map[string]string{
+		"username":   username,
+		"sshVersion": sshVersion,
 	}
 
 	// The incoming Request channel must be serviced.
 	go ssh.DiscardRequests(reqs)
 
 	// Service the incoming Channel channel.
-	handleChannels(chans)
-	// for newChannel := range chans {
-	// 	// Channels have a type, depending on the application level
-	// 	// protocol intended. In the case of a shell, the type is
-	// 	// "session" and ServerShell may be used to present a simple
-	// 	// terminal interface.
-	// 	if newChannel.ChannelType() != "session" {
-	// 		newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
-	// 		continue
-	// 	}
-	// 	channel, requests, err := newChannel.Accept()
-	// 	if err != nil {
-	// 		log.Fatalf("Could not accept channel: %v", err)
-	// 	}
+	handleChannels(chans, connectionDetails)
 
-	// 	// Sessions have out-of-band requests such as "shell",
-	// 	// "pty-req" and "env".  Here we handle only the
-	// 	// "shell" request.
-	// 	go func(in <-chan *ssh.Request) {
-	// 		for req := range in {
-	// 			fmt.Println(req)
-	// 			if req.Type == "shell" {
-	// 				err = req.Reply(true, nil)
-	// 				if err != nil {
-	// 					fmt.Println("bad shell reply")
-	// 				}
-	// 			}
-	// 			if req.Type == "pty-req" {
-
-	// 			}
-	// 		}
-	// 	}(requests)
-
-	// 	term := terminal.NewTerminal(channel, "> ")
-	// 	go func() {
-	// 		defer channel.Close()
-	// 		for {
-	// 			fmt.Println("prior ")
-	// 			line, err := term.ReadLine()
-	// 			fmt.Println("after")
-	// 			if err != nil {
-	// 				break
-	// 			}
-	// 			fmt.Println(line)
-	// 		}
-	// 	}()
-	// }
 }
--- tricks.go
+++ tricks.go
@@ -6,11 +6,12 @@ import (
 	"io"
 	"log"
 	"strings"
-	"text/scanner"
 	"time"
 
 	"golang.org/x/crypto/ssh"
 )
+
+var ubuntu1804LoginMessage = "\r\n Welcome to Ubuntu 18.04.1 LTS (GNU/Linux 4.9.125-linuxkit x86_64) \r\n\r\n * Documentation:  https://help.ubuntu.com \r\n * Management: https://landscape.canonical.com \r\n * Support:        https://ubuntu.com/advantage \r\n\r\n This system has been minimized by removing packages and content that are not required on a system that users do not log into.  \r\n\r\nTo restore this content, you can run the 'unminimize' command.  \r\n\r\nThe programs included with the Ubuntu system are free software; the exact distribution terms for each program are described in the individual files in /usr/share/doc/*/copyright.  \r\n\r\nUbuntu comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law."
 
 func gnomeKeychainTrick(channel ssh.Channel) {
 	io.WriteString(channel, "Unlock gnome-keyring\r\npassword: ")
@@ -35,50 +36,81 @@ func sudoPermissionsRequiredTrick(channel ssh.Channel) {
 }
 
 func signalsTrick(channel ssh.Channel) {
-	var s scanner.Scanner
-	s.Init(channel)
-	s.Mode = scanner.ScanChars
-	io.WriteString(channel, "ubuntu string")
+	scanner := bufio.NewScanner(channel)
+	io.WriteString(channel, ubuntu1804LoginMessage)
+	scanner.Split(bufio.ScanBytes)
 	var received strings.Builder
-
+	var tok string
 	for {
-		tok := s.Scan()
-
-		if tok == '\x03' {
+		scanner.Scan()
+		tok = scanner.Text()
+		// early breaks sigint or eof
+		if tok == "\x03" {
 			break
 		}
-		if tok == '\x04' {
+		if tok == "\x04" {
 			break
 		}
-		received.WriteRune(tok)
-		if len(received.String()) > 3 {
-			fmt.Println("blah", string(received.String()[len(received.String())-4:]))
+		// replace carriage returns with newlines
+		if tok == "\r" {
+			tok = "\n"
 		}
+		received.WriteString(tok)
 		if len(received.String()) > 3 &&
 			string(received.String()[len(received.String())-4:]) == "exit" {
 			break
 		}
-
-		fmt.Println(received.String())
-		fmt.Println(tok)
-		fmt.Printf("%s: %s\n", s.Position, s.TokenText())
-
-		fmt.Printf("%s: %s\n", s.Position, s.TokenText())
 	}
 
-	// io.WriteString(channel, "ubuntu string")
-	// bio := bufio.NewReader(channel)
+	fmt.Print(received.String())
+	log.Println(received.String())
+	io.WriteString(channel, "\r\nConnection to remote host ended \r\n/home/ $ ")
+	received.Reset()
+	// now we pretend to be the users local computer
+	pretendToBeUsersComputer(channel)
+}
 
-	// bio.ReadString('\x03')
-	// io.WriteString(channel, "\r\n/home/ $ ")
-	// // echo each character
-	// for {
-	// 	r, _, _ := bio.ReadRune()
-	// 	if r == '\r' {
-	// 		break
-	// 	}
-	// 	io.WriteString(channel, string(r))
-	// }
-	// io.WriteString(channel, "\r\n")
+func pretendToBeUsersComputer(channel ssh.Channel) {
+	scanner := bufio.NewScanner(channel)
+	scanner.Split(bufio.ScanBytes)
+	var received strings.Builder
+	var tok string
+
+	for {
+		scanner.Scan()
+		tok = scanner.Text()
+		// early breaks sigint or eof
+
+		if tok == "\x03" {
+			continue
+		}
+
+		if tok == "\x04" {
+			continue
+		}
+
+		// echo the users keystrokes back to them
+		io.WriteString(channel, tok)
+		//
+		received.WriteString(tok)
+		if tok == "\r" {
+			// get commands being executed
+			log.Println(received.String())
+			command := received.String()
+			commandParts := strings.Fields(command)
+
+			if len(commandParts) > 0 {
+				if commandParts[0] == "sudo" {
+					io.WriteString(channel, "\r\n sudo password:")
+				} else {
+					permissionDeniedStr := fmt.Sprintf("\r\npermission denied: %s", commandParts[0])
+					io.WriteString(channel, permissionDeniedStr)
+				}
+			}
+
+			io.WriteString(channel, "\r\n/home/ $ ")
+		}
+
+	}
 
 }
